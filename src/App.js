46f2aadc64fd1e854ebdb49e08cc4de98437177b
Carlos Aviña <juan.avina@wizeline.com>
2022-02-02 21:18:59 -0600

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import kid from "./media/smug.gif";
+import music from "./media/music.wav";
+import "./App.css";
 
 function App() {
+  const [start, setStart] = React.useState(false);
+  const audioPlayer = React.useRef(null);
+
+  function onPlay() {
+    setStart(true);
+
+    if (audioPlayer) {
+      audioPlayer.current.play();
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!start ? (
+        <button onClick={onPlay}>Smug</button>
+      ) : (
+        <div className="App-Grid">
+          <img
+            className="App-Wallpaper"
+            src={`/wallpaper${8}.gif`}
+            alt="wallpaper"
+          />
+          <img className="App-Kid" src={kid} alt="logo" />
+        </div>
+      )}
+      <audio
+        className="App-Audio"
+        ref={audioPlayer}
+        src={music}
+        loop
+      />
     </div>
   );
 }
