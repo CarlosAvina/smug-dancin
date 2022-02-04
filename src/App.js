ef05cc75ce2af65a77476a100d08763137e8bce9
Carlos Aviña <juan.avina@wizeline.com>
2022-02-03 19:42:14 -0600

    Add functionality to side menu with wallpaper selection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+
+import SideMenu from "./components/SideMenu";
+import Thumbnail from "./components/Thumbnail";
+
 import kid from "./media/smug.gif";
 import music from "./media/music.wav";
 import "./App.css";
 
+const wallpapers = [1, 2, 3, 4, 5, 6, 7, 8];
+
 function App() {
   const [start, setStart] = React.useState(false);
+  const [sideMenu, setSideMenu] = React.useState(false);
+  const [wallpaper, setWallpaper] = React.useState(1);
   const audioPlayer = React.useRef(null);
 
   function onPlay() {
@@ -15,25 +23,54 @@ function App() {
     }
   }
 
+  function toggleSideMenu() {
+    setSideMenu(!sideMenu);
+  }
+
+  function changeWallpaper(event) {
+    const wallpaper = Number(event.currentTarget.id);
+    setWallpaper(wallpaper);
+  }
+
   return (
     <div className="App">
       <div className="App-Grid">
         {!start ? (
-          <button className="App-Button" onClick={onPlay}>
+          <button className="App-Button App-Button-Start" onClick={onPlay}>
             dance
           </button>
         ) : (
           <>
             <img
               className="App-Wallpaper"
-              src={`/wallpaper${1}.gif`}
+              src={`/wallpaper${wallpaper}.gif`}
               alt="wallpaper"
             />
             <img className="App-Kid" src={kid} alt="logo" />
+            <button
+              className="App-Button App-Button-Wallpaper"
+              onClick={toggleSideMenu}
+            >
+              Background
+            </button>
           </>
         )}
       </div>
-      <audio className="App-Audio" ref={audioPlayer} src={music} loop />
+      <audio className="App-Audio" ref={audioPlayer} src={music} loop muted />
+      {sideMenu && (
+        <SideMenu>
+          <button onClick={toggleSideMenu}>Close</button>
+          {wallpapers.map((item) => (
+            <Thumbnail
+              id={item}
+              key={item}
+              src={`/wallpaper${item}.gif`}
+              selected={item === wallpaper}
+              onClick={changeWallpaper}
+            />
+          ))}
+        </SideMenu>
+      )}
     </div>
   );
 }
