427b7c883fc323f6a1ab7e83a7c813830cc254c2
Carlos Aviña <juan.avina@wizeline.com>
2022-03-01 19:08:04 -0600

    Add styles to tooltip

--- src/components/MenuButton/index.js
+++ src/components/MenuButton/index.js
@@ -3,15 +3,18 @@ import Tooltip from "@reach/tooltip";
 import styles from "./MenuButton.module.css";
 import "@reach/tooltip/styles.css";
 
+const tooltipStyles = {
+  backgroundColor: "transparent",
+  color: "white",
+  filter:
+    "brightness(0) saturate(100%) invert(97%) sepia(36%) saturate(321%) hue-rotate(51deg) brightness(101%) contrast(101%) drop-shadow(0 0 10px #42ff4f)",
+};
+
 const MenuButton = ({ label, onClick, image, alt, imageStyles }) => {
   return (
-    <Tooltip label={label}>
+    <Tooltip style={tooltipStyles} label={label}>
       <button className={styles.button} onClick={onClick}>
-        <img
-          src={image}
-          alt={alt}
-          className={cx(imageStyles, styles.filter)}
-        />
+        <img src={image} alt={alt} className={cx(imageStyles, styles.filter)} />
       </button>
     </Tooltip>
   );
